6283c3946da36af8e589e1e8dd52e4c41114865c
vinaykumar.kowkoori <vinaykumar.kowkoori@LP1-AP-51820446.PROD.HCLPNP.COM>
2020-05-11 05:15:00 +0530

    data types

--- identifiers/Identifiers.java
+++ identifiers/Identifiers.java
@@ -19,6 +19,16 @@ public class Identifiers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			
+			System.out.println("Vinay");
+			System.out.println(10/0);
+		}catch(ArithmeticException e) {
+			System.out.println("Hello");
+		}catch(Exception e) {
+			System.out.println("Hello");
+		}
+		
 
 	}
 
